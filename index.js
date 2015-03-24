1cffaeee3a95feae55d54067c77c188ac690143f
Moshe Njema <moshe@localhost.localdomain>
2015-03-24 15:35:51 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+
+var app = express();
+
+var handlebars = require('express3-handlebars')
+	.create({defaultLayout:'main'});
+app.engine ('handlebars', handlebars.engine);
+app.set('view engine','handlebars');
+
+
+app.set('port', process.env.PORT || 4000);
+
+app.get('/', function(req,res){
+	res.render('home');
+
+});
+
+app.get('/about',function(req,res){
+	res.render('about');
+});
+
+app.use (function(req,res){
+	res.status(404);
+	res.render('404');
+
+});
+
+app.use (function (err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+
+
+})
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:') +
+		app.get ('port') + ('; Press Ctrl +C to terminate');
+})
