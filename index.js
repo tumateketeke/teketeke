7d6cc775a9edb8a5df8fdc381e624f60518995f5
nj3ma <m.njema@gmail.com>
2015-04-04 00:15:38 +0300

    Adds testing

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ app.set('view engine','handlebars');
 
 app.set('port', process.env.PORT || 4000);
 
+app.use(function(req, res, next){
+    res.locals.showTests == app.get('env') !=='production') &&
+        req.query.test === 1;
+    next();
+    })
+
 app.use(express.static( __dirname + '/public'));
+
 app.get('/', function(req,res){
 	res.render('home');
 
